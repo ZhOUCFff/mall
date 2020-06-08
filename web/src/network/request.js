358da863c1c677080ea72ef3b4e4be4dd4a6078e
ZhOUCFff <1757775936@qq.com>
2020-06-08 17:54:43 +0800

    修改接口地址

--- web/src/network/request.js
+++ web/src/network/request.js
@@ -3,79 +3,91 @@ import axios from 'axios'
 export function request1(config) {
   const instance = axios.create({
     baseURL: 'http://123.207.32.32:8000',
-    timeout: 5000
+    timeout: 5000,
   })
 
-  instance.interceptors.request.use(config => {
-    return config
-  }, err => {
+  instance.interceptors.request.use(
+    config => {
+      return config
+    },
+    err => {}
+  )
 
-  })
-
-  instance.interceptors.response.use(res => {
-    return res.data
-  }, err => {
-
-  })
+  instance.interceptors.response.use(
+    res => {
+      return res.data
+    },
+    err => {}
+  )
 
   return instance(config)
 }
 
 export function request2(config) {
   const instance = axios.create({
-    baseURL: 'http://106.54.54.237:8000/api/h8',
-    timeout: 5000
+    baseURL: 'http://106.54.54.237:8000/api/m3',
+    timeout: 5000,
   })
 
-  instance.interceptors.request.use(config => {
-    return config
-  }, err => {
+  instance.interceptors.request.use(
+    config => {
+      return config
+    },
+    err => {}
+  )
 
-  })
-
-  instance.interceptors.response.use(res => {
-    return res.data
-  }, err => {
-
-  })
+  instance.interceptors.response.use(
+    res => {
+      return res.data
+    },
+    err => {}
+  )
 
   return instance(config)
 }
 
 export function request3(config) {
   const instance = axios.create({
-    baseURL: 'http://123.207.32.32:8000/api/h8',
-    timeout: 5000
+    baseURL: 'http://123.207.32.32:8000/api/m3',
+    timeout: 5000,
   })
   //请求拦截
-  instance.interceptors.request.use(config => {
-    return config
-  }, err => {
-  })
+  instance.interceptors.request.use(
+    config => {
+      return config
+    },
+    err => {}
+  )
   //响应拦截
-  instance.interceptors.response.use(res => {
-    return res.data.result
-  }, err => {
-  })
+  instance.interceptors.response.use(
+    res => {
+      return res.data.result
+    },
+    err => {}
+  )
   //发送真正的网络请求
   return instance(config)
 }
 
 export function request4(config) {
   const instance = axios.create({
-    baseURL: 'http://123.207.32.32:8000/api/h8',
-    timeout: 5000
+    baseURL: 'http://123.207.32.32:8000/api/m3',
+    timeout: 5000,
   })
   //请求拦截
-  instance.interceptors.request.use(config => {
-    return config
-  }, err => {
-  })
+  instance.interceptors.request.use(
+    config => {
+      return config
+    },
+    err => {}
+  )
   //响应拦截
-  instance.interceptors.response.use(res => {
-    return res.data
-  }, err => {
-  })
+  instance.interceptors.response.use(
+    res => {
+      return res.data
+    },
+    err => {}
+  )
   //发送真正的网络请求
   return instance(config)
 }
